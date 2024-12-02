75942b616dc56bfcd671dd46b69f43ec45fcd406
Gustavo Santos <gurezendes@gmail.com>
2024-12-02 17:28:55 -0500

    Upd: added html save to .data

--- flightprices.py
+++ flightprices.py
@@ -12,6 +12,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+import datetime
 import time
 
 # Opening Driver to use the pointer in browser
@@ -40,12 +41,22 @@ driver.find_element(By.CSS_SELECTOR, "#datepicker_temp1").send_keys(Keys.RETURN)
 
 time.sleep(1)
 
-# Complete DATES
-start_date = driver.find_element(By.CSS_SELECTOR, "#startDate")
-start_date.send_keys("30/10/2024") #dd/mm/yyyy
+### Complete DATES
 
+# Start Date
+start_date = driver.find_element(By.CSS_SELECTOR, "#startDate")
+current_time = datetime.datetime.now()
+if current_time.day < 10:
+    day_ = f'0' + str(current_time.day)
+else:
+    day_ = current_time. day
+month_ = current_time.month
+year_ = current_time.year
+start_date.send_keys(f"{day_}/{month_}/{year_}") #dd/mm/yyyy
+
+# End Date = Start + 10
 end_date = driver.find_element(By.ID, "endDate")
-end_date.send_keys("12/11/2024") #dd/mm/yyyy
+end_date.send_keys(f"{current_time.day + 10}/{month_}/{year_}") #dd/mm/yyyy
 
 # Click Button "Fechar"
 driver.find_element(By.CSS_SELECTOR, 'button[tabindex="0"]').click()
@@ -58,18 +69,14 @@ time.sleep(25)
 
 # Get page HTML
 html_doc = driver.page_source
-soup = BeautifulSoup(html_doc, 'html.parser')
-
-# Get text only
-page = soup.get_text()
-print(page)
+soup = BeautifulSoup(html_doc, 'html.parser').prettify()
 
 # Close Chrome driver
 driver.quit()
 
 # Save file
 
-file = open('/data/flights.html', 'w')
+file = open('.data/flights.html', 'w')
 file.write(soup)
 file.close()
 
--- time.py
+++
@@ -1,6 +0,0 @@
-import datetime
-
-
-
-
-print(f'Today is {day_}/{month_}/{year_}')
